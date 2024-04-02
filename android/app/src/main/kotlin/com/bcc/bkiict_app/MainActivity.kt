22e4e15e2ab054b3acbc61892d500abb863ac378
Mahfuz-00 <sayembinabdullah@gmail.com>
2024-04-02 22:31:35 +0600

    Last Updated: 2nd April 2024

---
+++ android/app/src/main/kotlin/com/bcc/bkiict_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bcc.bkiict_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'Splashscreen UI/splashscreenUI.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Color.fromRGBO(25, 192, 122, 1), // Change the status bar color here
+    ));
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BKIICT',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(25, 192, 122, 1)),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
