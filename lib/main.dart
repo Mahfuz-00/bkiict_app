b6ea413127e980ebbe681953e15a5f86775354d3
Mahfuz-00 <sayembinabdullah@gmail.com>
2024-05-27 12:26:57 +0600

    Last Updated: 27 May 2024

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'Splashscreen UI/splashscreenUI.dart';
+import 'package:rename_app/rename_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'BKIICT',
+      title: 'BKIICT Student App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(134, 188, 66, 1)),
         useMaterial3: true,
